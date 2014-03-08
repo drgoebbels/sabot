8b1cc529abe43ef3b39bf49aee2ee46381bd0777
Paula Deen <prtskull13@yahoo.com>
2014-03-07 23:09:27 -0600

    Added macro constants for server IPs

--- sanet.h
+++ sanet.h
@@ -1,4 +1,24 @@
 #ifndef SANET_H_
 #define SANET_H_
 
+#define PORT                1138
+
+#define S_REGISTER          "67.19.145.10"
+
+#define S_2D_CENTRAL        "74.86.43.9"
+#define S_PAPER_THIN        "74.86.43.8"
+#define S_FINE_LINE         "67.19.138.234" //(?)
+#define S_U_OF_SA           "67.19.138.236"
+#define S_FLAT_WORLD        "74.86.3.220"
+#define S_PLANAR_OUTPOST    "67.19.138.235"
+#define S_MOBIUS_METROPOLIS "74.86.3.221"
+#define S_AMSTERDAM         "94.75.214.10"
+#define S_COMPATABILITY     "74.86.3.222"
+#define S_QUICKSTART        "67.19.138.236"
+#define S_SS_LINEAGE        "74.86.43.10"
+
+#define S_IN_USE            S_2D_CENTRAL
+
+
+
 #endif
