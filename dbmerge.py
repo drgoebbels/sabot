8a8e07e4d1bd4394be313645cc17eb05aae80068
Paula Deen <prtskull13@yahoo.com>
2014-05-19 16:46:39 -0500

    Added code to connect to databases being merge.

--- dbmerge.py
+++ dbmerge.py
@@ -1,18 +1,41 @@
 #Script for merging sabot databases 
 
+import os
 import sys
 import sqlite3
 
+def mergeDB(dbName):
+    dbCon = None
+    try:
+        if os.path.isfile(dbName):
+            dbCon = sqlite3.connect(dbName)
+            db = dbCon.cursor()
+        else:
+            sys.stderr.write('Database %s does not exist\n' % dbName)
+    except:
+        sys.stderr.write('Failed to access Database: %s\n' % dbName)
+    finally:
+        if dbCon:
+            dbCon.close()
 
 if len(sys.argv) < 2:
-    sys.stderr.write('No database specified to merge')
+    sys.stderr.write('No database specified to merge\n')
 else:
+    sadbCon = None
     try:
-        sadbCon = sqlite3.connect('sabot.db')
-        sadb = sadbCon.cursor()
-    except line.Error, e:
-        sys.stderr.write('Failed to Connect to Database')
+        if os.path.isfile('sabot.db'):
+            sadbCon = sqlite3.connect('sabot.db')
+            sadb = sadbCon.cursor()
+        
+            for arg in sys.argv[1:]:
+                mergeDB(arg)
+        else:
+            sys.stderr.write('Failed to access Database sabot.db\n')
+            sys.exit(1) 
+    except sqlite3.Error, e:
+        sys.stderr.write('Failed to access Database sabot.db\n')
         sys.exit(1)
-         
+    finally:
+        if sadbCon:
+            sadbCon.close()
 
-    
